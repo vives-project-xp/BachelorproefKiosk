bf18a68eadad5dd631f77f7ed567558ffda22abe
SampersS <seraphin.sampers@hotmail.com>
2024-02-10 20:33:16 +0100

    wha

---
+++ backend/db.js
@@ -0,0 +1,120 @@
+const dotenv = require("dotenv")
+const mysql = require("mysql")
+
+let configConnect = (returnCode) => {
+    dotenv.config();
+    const dbhost = process.env.DB_HOST;
+    const dbuser = process.env.DB_USER;
+    const dbpass = process.env.DB_PASS;
+    const database = process.env.DB_DTBS;
+
+    let connection = mysql.createConnection({
+        host: dbhost,
+        user: dbuser,
+        password: dbpass,
+        database: database,
+    });
+    connection.connect(error => {
+        if(error){
+            console.log("error: Unable to connect to database.",error.message)
+        }else{
+            console.log("Verbonden met succes.")
+            returnCode(connection)
+        }
+    });
+}
+const getFiche = (req, res,id) => {
+    configConnect(function(connection){
+        const queryry = "SELECT * FROM fiche where id = ?"
+        connection.query(queryry, [id], (err, data) => {
+        if(err){
+            res.status(404).send("interne database error")
+            console.log("interne database error")
+        }else{
+            res.status(200).send(data);
+        }
+        connection.end()
+    });})
+}
+const getFiches = (req, res, richtingId) => {
+    configConnect(function(connection){
+        var queryry = "SELECT * FROM fiche"
+        if(richtingId!=undefined){
+            queryry+=" where richtingId=?"
+        }
+        connection.query(queryry,[richtingId], (err, data) => {
+            if(err){
+                res.status(404).send("interne database error")
+                console.log("interne database error")
+            }else{
+                res.status(200).send(data);
+            }
+            connection.end()
+        });})
+}
+const addFiche = (req, res, objectData)=>{
+    configConnect(function(connection){
+        const queryry = "insert into fiche values(NULL, ?,?,?,?,?,?,?,?,?)"
+        connection.query(queryry,[objectData.studentNaam, objectData.bedrijf, objectData.titel,objectData.link,objectData.tekst, objectData.afbeelding1,objectData.afbeelding2,objectData.hashtags,objectData.richtingId], (err, data) => {
+            if(err){
+                res.status(404).send("interne database error")
+                console.log("interne database error")
+            }else{
+                res.status(200).send("ok");
+            }
+            connection.end()
+        });})
+}
+const removeFiche = (req, res, id)=>{
+    configConnect(function(connection){
+        const queryry = "delete from fiche where id=?"
+        connection.query(queryry,[id], (err, data) => {
+            if(err){
+                res.status(404).send("interne database error")
+                console.log("interne database error")
+            }else{
+                res.status(200).send("ok");
+            }
+            connection.end()
+        });})
+}
+const getRichtingen = (req, res) => {
+    configConnect(function(connection){
+        const queryry = "SELECT * FROM richting"
+        connection.query(queryry, (err, data) => {
+            if(err){
+                res.status(404).send("interne database error")
+                console.log("interne database error")
+            }else{
+                res.status(200).send(data);
+            }
+            connection.end()
+        });})
+}
+const addRichting = (req, res, naam)=>{
+    configConnect(function(connection){
+        const queryry = "insert into richting values(NULL, ?)"
+        connection.query(queryry,[naam], (err, data) => {
+            if(err){
+                res.status(404).send("interne database error")
+                console.log("interne database error")
+            }else{
+                res.status(200).send("ok");
+            }
+            connection.end()
+        });})
+}
+const removeRichting = (req, res, id)=>{
+    configConnect(function(connection){
+        const queryry = "delete from richting where id=?"
+        connection.query(queryry,[id], (err, data) => {
+            if(err){
+                res.status(404).send("interne database error")
+                console.log("interne database error")
+            }else{
+                res.status(200).send("ok");
+            }
+            connection.end()
+        });})
+}
+module.exports = {getRichtingen, getFiche, getFiches, addRichting,addFiche, removeFiche, removeRichting}
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,42 @@
 const express = require("express")
 const port = 3000
 const cors = require("cors")
+const db = require("./db")
 
 const app = express()
 app.use(cors());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true}));
 
 app.get('/backend/', (req, res) => {
     res.send('Verbinding met de backend is werkend!')
 })
-
+app.get('/backend/getFiche/:id', (req, res) => {
+    db.getFiche(req,res, req.params.id)
+})
+app.get('/backend/getFiches/:richtingid', (req, res) => {
+    //als richiting id een nummer is dan worden alle fiches van deze richting terug gestuurd, anders alles
+    if(!isNaN(req.params.richtingid - 0)){
+        db.getFiches(req,res,req.params.richtingid)
+    }else{
+        db.getFiches(req,res)
+    }
+})
+app.get('/backend/addFiche', (req, res) => {
+    db.addFiche(req,res, req.body)
+})
+app.get('/backend/removeFiche/:id', (req, res) => {
+    db.removeFiche(req,res, req.params.id)
+})
+app.get('/backend/getRichtingen', (req, res) => {
+    db.getRichtingen(req,res)
+})
+app.get('/backend/addRichting/:naam', (req, res) => {
+    db.addRichting(req,res,req.params.naam)
+})
+app.get('/backend/removeRichting/:id', (req, res) => {
+    db.removeRichting(req,res, req.params.id)
+})
 app.listen(port, () => {
     console.log(`Backend app listening on port ${port}`)
 })

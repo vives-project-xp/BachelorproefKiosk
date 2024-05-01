ee3c22db52e4c4cd73fb381430c4870855a78a73
SampersS <seraphin.sampers@hotmail.com>
2024-05-01 21:05:27 +0200

    kopie script

--- server scraping/python.py
+++ server scraping/python.py
@@ -1,4 +1,73 @@
-# nu nog input maar later leest hij dit uit uit een bestand
-serverip = input("server ip")
-wp_gebruiker = input("gebruikersnaam")
-wp_wachtwoord = input("wachtwoord")
+# input wordt later afgehaald van docker
+import mysql.connector
+import os
+import shutil
+import requests
+
+serverip = "172.18.0.4"
+db_gebruiker = "exampleuser"
+db_wachtwoord = "examplepass"
+db_db = "exampledb"
+
+localip = "172.18.0.4"
+loc_gebruiker = "exampleuser"
+loc_wachtwoord = "examplepass"
+loc_db = "exampledb"
+
+db = mysql.connector.connect(
+  host=serverip,
+  user=db_gebruiker,
+  password=db_wachtwoord,
+  database=db_db
+)
+locdb = mysql.connector.connect(
+  host=localip,
+  user=loc_gebruiker,
+  password=loc_wachtwoord,
+  database=loc_db
+)
+local_cursor = locdb.cursor()
+cursor = db.cursor()
+
+siteurl = ""
+try:
+   local_cursor.execute("SELECT * FROM `wp_options` WHERE option_id=1;")
+except:
+   print("error met lokale database")
+   exit(1)
+
+querry_res = local_cursor.fetchall()
+siteurl = querry_res[0][2]
+
+
+try:
+   cursor.execute("SELECT * FROM `wp_posts` WHERE post_type='attachment';")
+except:
+   print("Error: unable to fecth data")
+   exit(1)
+results = cursor.fetchall()
+for row in results:
+    #print(row) = (98, 1, datetime.datetime(2024, 5, 1, 16, 25, 10), datetime.datetime(2024, 5, 1, 16, 25, 10), '', 'arcade', '', 'inherit', 'open', 'closed', '', 'arcade', '', '', datetime.datetime(2024, 5, 1, 16, 25, 10), datetime.datetime(2024, 5, 1, 16, 25, 10), '', 0, 'http://localhost:8080/wp-content/uploads/2024/05/arcade-1.gif', 0, 'attachment', 'image/gif', 0)
+    url = row[18]
+    filename = url.split('/')[-1]
+    path = "./downloads/"+filename
+    with requests.get(url, stream=True) as r:
+            with open(path, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+    lst = list(row)
+    lst[18] = siteurl + "/wp-content/downloads/" + filename
+    lst = lst[1:]
+    lst[1] = lst[1].strftime("%Y-%m-%d %H:%M:%S")
+    lst[2] = lst[2].strftime("%Y-%m-%d %H:%M:%S")
+    lst[13] = lst[13].strftime("%Y-%m-%d %H:%M:%S")
+    lst[14] = lst[14].strftime("%Y-%m-%d %H:%M:%S")
+    
+    row = tuple(lst)
+    try:
+       stri = "INSERT INTO wp_posts (post_author,post_date,post_date_gmt,post_content,post_title,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count) Values" + str(row)
+       print(stri)
+       #local_cursor.execute(string)
+    except:
+       print("Error bij updaten database")
+       exit(1)
+db.close()

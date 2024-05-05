b75a078cb9865d8d6858360cc60f4d0c04321d2e
SampersS <seraphin.sampers@hotmail.com>
2024-05-05 12:51:45 +0200

    efficienter synchroniseren

--- server scraping/py-project/python.py
+++ server scraping/py-project/python.py
@@ -36,16 +36,20 @@ def quit():
 
 local_cursor = locdb.cursor()
 cursor = db.cursor()
-
+siteurl = ""
+filenames = []
 try:
    local_cursor.execute("SELECT * FROM `wp_options` WHERE option_id=1;")
+   querry_res = local_cursor.fetchall()
+   siteurl = querry_res[0][2]
+   local_cursor.execute("SELECT * FROM `wp_posts` WHERE post_type='attachment';")
+   results = cursor.fetchall()
+   for row in results:
+      url = row[18]
+      filenames.append(url.split('/')[-1])
 except:
    print("error met lokale database")
    quit()
-
-querry_res = local_cursor.fetchall()
-siteurl = querry_res[0][2]
-
 
 try:
    cursor.execute("SELECT * FROM `wp_posts` WHERE post_type='attachment';")
@@ -57,26 +61,34 @@ for row in results:
     #print(row) = (98, 1, datetime.datetime(2024, 5, 1, 16, 25, 10), datetime.datetime(2024, 5, 1, 16, 25, 10), '', 'arcade', '', 'inherit', 'open', 'closed', '', 'arcade', '', '', datetime.datetime(2024, 5, 1, 16, 25, 10), datetime.datetime(2024, 5, 1, 16, 25, 10), '', 0, 'http://localhost:8080/wp-content/uploads/2024/05/arcade-1.gif', 0, 'attachment', 'image/gif', 0)
     url = row[18]
     filename = url.split('/')[-1]
-    path = "./downloads/"+filename
-    with requests.get(url, stream=True) as r:
-            with open(path, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-    lst = list(row)
-    lst[18] = siteurl + "/wp-content/downloads/" + filename
-    lst = lst[1:]
-    lst[1] = lst[1].strftime("%Y-%m-%d %H:%M:%S")
-    lst[2] = lst[2].strftime("%Y-%m-%d %H:%M:%S")
-    lst[13] = lst[13].strftime("%Y-%m-%d %H:%M:%S")
-    lst[14] = lst[14].strftime("%Y-%m-%d %H:%M:%S")
-    
-    row = tuple(lst)
-    try:
-       stri = "INSERT INTO wp_posts (post_author,post_date,post_date_gmt,post_content,post_title,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count) Values" + str(row)
-       local_cursor.execute(stri)
-       print(stri)
-       print("upload van bestand",filename)
-    except:
-       print("Error bij updaten database")
-       quit()
+    ind = filenames.index(filename)
+    if(ind != -1):
+      del filenames[ind]
+      path = "./downloads/"+filename
+      with requests.get(url, stream=True) as r:
+               with open(path, 'wb') as f:
+                  shutil.copyfileobj(r.raw, f)
+      lst = list(row)
+      lst[18] = siteurl + "/wp-content/downloads/" + filename
+      lst = lst[1:]
+      lst[1] = lst[1].strftime("%Y-%m-%d %H:%M:%S")
+      lst[2] = lst[2].strftime("%Y-%m-%d %H:%M:%S")
+      lst[13] = lst[13].strftime("%Y-%m-%d %H:%M:%S")
+      lst[14] = lst[14].strftime("%Y-%m-%d %H:%M:%S")
+      
+      row = tuple(lst)
+      try:
+         stri = "INSERT INTO wp_posts (post_author,post_date,post_date_gmt,post_content,post_title,post_excerpt,post_status,comment_status,ping_status,post_password,post_name,to_ping,pinged,post_modified,post_modified_gmt,post_content_filtered,post_parent,guid,menu_order,post_type,post_mime_type,comment_count) Values" + str(row)
+         local_cursor.execute(stri)
+         print(stri)
+         print("upload van bestand",filename)
+      except:
+         print("Error bij updaten database")
+         quit()
+locdb.commit()
+#degene die overbleven in de filenames lijst zijn bestanden die al verwijdert waren
+for bestand in filenames:
+   local_cursor.execute("DELETE from wp_posts where post_type='attachment' and guid like '%"+"downloads/"+ bestand +"'")
+   os.remove("./downloads/"+bestand)
 locdb.commit()
 quit()
